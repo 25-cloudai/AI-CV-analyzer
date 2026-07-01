7692c055822d938bf418a2b319bd804de0712a46
coding-spec <vanukriti6@gmail.com>
2026-07-01 18:36:06 +0530

    Authentication

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from "react-router";
+import ScoreCircle from "~/components/ScoreCircle";
+type Resume = {
+    id: string;
+    companyName: string;
+    jobTitle: string;
+    feedback: {
+        overallScore: number;
+    };
+    imagePath: string;
+};
+const ResumeCard = ({ resume }: { resume: resume }) => {
+    return (
+    <Link to={`/resume/${resume.id}`} className="resume-card animate-in fade-in duration-1000">
+        <div className="resume-card-header">
+        <div className="flex flex-col gap-2">
+            <h2 className="!text-black font-bold break-words">
+                {resume.companyName}
+            </h2>
+            <h3 className="text-lg break-words text-gray-500">
+                {resume.jobTitle}
+            </h3>
+        </div>
+        <div className="flex-shrink-0">
+            <ScoreCircle score={resume.feedback.overallScore}/>
+        </div>
+        </div>
+        <div className="gradient-border animate-in fade-in duration-1000">
+            <div className="w-full h-full">
+                <img
+                    src={resume.imagePath}
+                    alt="resume"
+                    className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"/>
+            </div>
+        </div>
+    </Link>
+);
+};
+export default ResumeCard;
